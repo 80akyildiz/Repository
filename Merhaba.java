801c03172d9387509d5cccefed5d5df6bdabe2a7
80akyildiz <80akyildiz@gmail.com>
2024-08-26 16:37:42 +0300

    Add files via upload

---
+++ Merhaba.java
@@ -0,0 +1,11 @@
+package Gun1;
+
+public class Merhaba {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba 1");
+
+        System.out.println("Merhaba 2");
+
+    }
+}
